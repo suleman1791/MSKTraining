448235368fe54dc55c835e562bbb61f248f8af79
suleman1791 <mskkhan1791@gmail.com>
2022-02-02 19:15:32 +0530

    2

---
+++ MSKTraining/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSKTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sowmya is a bad teacher");
+            Console.ReadLine();
+        }
+    }
+}
